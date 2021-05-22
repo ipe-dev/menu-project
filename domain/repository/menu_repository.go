fd4f901894b592a5bbb2bef351d2b6d56a97f2c1
ipe <izaki.yb1998@gmail.com>
2021-05-23 08:03:42 +0900

    usecase

--- domain/repository/menu_repository.go
+++ domain/repository/menu_repository.go
@@ -2,13 +2,12 @@ package repository
 
 import (
 	"github.com/ipe-dev/menu_project/domain/entity"
-	"github.com/ipe-dev/menu_project/usecase"
 )
 
 type MenuRepository interface {
 	Create(m *entity.Menu) error
 	Update(m *entity.Menu) error
-	Get(r usecase.GetMenuRequest) (*entity.Menu, error)
-	GetList(r usecase.GetMenuListRequest) ([]*entity.Menu, error)
+	Get(id int) (*entity.Menu, error)
+	GetList(weekID int) ([]entity.Menu, error)
 	Delete(id int) error
 }
--- usecase/menu_usecase.go
+++ usecase/menu_usecase.go
@@ -7,20 +7,42 @@ import (
 )
 
 type MenuUseCase interface {
-	CreateMenu(c *gin.Context) error
+	CreateMenu(CreateMenuRequest) error
+	GetMenu(c *gin.Context) (*entity.Menu, error)
 }
 type menuUseCase struct {
 	menuRepository repository.MenuRepository
 }
+type CreateMenuRequest struct {
+	Name   string `json:"name"`
+	Date   string `json:"date"`
+	Kind   int    `json:"kind"`
+	URL    string `json:"url"`
+	UserID int    `json:"user_id"`
+}
+type GetMenuListRequest struct {
+	StartDate int `json:"start_date"`
+	UserID    int `json:"user_id"`
+}
+type GetMenuRequest struct {
+	ID int `json:"id"`
+}
+type GetListRequest struct {
+	Date int `json:"date"`
+}
+
+func newMenu()
 
 func NewMenuUseCase(r repository.MenuRepository) MenuUseCase {
 	return &menuUseCase{r}
 }
 
-func (mu menuUseCase) CreateMenu(c *gin.Context) error {
-	var menu entity.Menu
-	c.BindJSON(&menu)
-
-	err := mu.menuRepository.Create(&menu)
+func (mu menuUseCase) CreateMenu(mr CreateMenuRequest) error {
+	err := mu.menuRepository.Create(mr)
 	return err
+}
+
+func (mu menuUseCase) GetMenu(c *gin.Context) (*entity.Menu, error) {
+
+	return
 }
