68e25e33561f42b9126ed1f0deddb641a367f481
ipe <izaki.yb1998@gmail.com>
2021-07-04 21:53:58 +0900

    persistence

--- infrastructure/persistence/menu.go
+++ infrastructure/persistence/menu.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ipe-dev/menu_project/domain/entity"
 	"github.com/ipe-dev/menu_project/domain/repository"
+	"github.com/ipe-dev/menu_project/errors"
 	"github.com/ipe-dev/menu_project/infrastructure/database"
 )
 
@@ -18,10 +19,10 @@ func (p menuPersistence) BulkCreate(menus []entity.Menu) ([]entity.Menu, error) 
 	err := tx.Model(&menus).Create(menus).Error
 	if err != nil {
 		tx.Rollback()
-		return menus, err
+		return menus, errors.NewInfraError(err, menus)
 	}
 	tx.Commit()
-	return menus, err
+	return menus, nil
 }
 func (p menuPersistence) BulkUpdate(menus []entity.Menu) ([]entity.Menu, error) {
 	tx := database.Db.Begin()
@@ -31,29 +32,29 @@ func (p menuPersistence) BulkUpdate(menus []entity.Menu) ([]entity.Menu, error) 
 		err = tx.Model(&menu).Updates(menu).Error
 		if err != nil {
 			tx.Rollback()
-			return res, err
+			return res, errors.NewInfraError(err, menu)
 		}
 		res = append(res, menu)
 	}
 	tx.Commit()
-	return res, err
+	return res, nil
 }
 func (p menuPersistence) GetByID(id int) (entity.Menu, error) {
 	var menu entity.Menu
 	Db := database.Db
 	err := Db.Table("menus").Where("id = ?", id).First(&menu).Error
-	return menu, err
+	return menu, errors.NewInfraError(err, id)
 }
 func (p menuPersistence) GetByDate(date int64, userID int) (entity.Menu, error) {
 	var menu entity.Menu
 	Db := database.Db
 	stringDate := time.Unix(date, 0).Format("2006/01/02 15:05:05")
 	err := Db.Table("menus").Where("date = ?", stringDate).Where("user_id = ?", userID).First(&menu).Error
-	return menu, err
+	return menu, errors.NewInfraError(err, stringDate, userID)
 }
 func (p menuPersistence) GetList(weekID int, userID int) ([]entity.Menu, error) {
 	var menus []entity.Menu
 	Db := database.Db
 	err := Db.Where("week_id = ?", weekID).Where("user_id = ?", userID).Find(&menus).Error
-	return menus, err
+	return menus, errors.NewInfraError(err, weekID, userID)
 }
--- infrastructure/persistence/user.go
+++ infrastructure/persistence/user.go
@@ -1,11 +1,10 @@
 package persistence
 
 import (
-	"log"
-
 	"github.com/ipe-dev/menu_project/domain/entity"
 	"github.com/ipe-dev/menu_project/domain/entity/value"
 	"github.com/ipe-dev/menu_project/domain/repository"
+	"github.com/ipe-dev/menu_project/errors"
 	"github.com/ipe-dev/menu_project/infrastructure/database"
 )
 
@@ -20,7 +19,7 @@ func (p userPersistence) Create(User entity.User) error {
 	err := tx.Table("users").Create(&User).Error
 	if err != nil {
 		tx.Rollback()
-		return err
+		return errors.NewInfraError(err, User)
 	}
 	var WeekID struct {
 		UserID int
@@ -31,46 +30,42 @@ func (p userPersistence) Create(User entity.User) error {
 	err = tx.Table("week_ids").Create(&WeekID).Error
 	if err != nil {
 		tx.Rollback()
-		return err
+		return errors.NewInfraError(err, WeekID)
 	}
 	tx.Commit()
-	return err
+	return nil
 }
 func (p userPersistence) Update(User entity.User) error {
 	tx := database.Db.Begin()
 	err := tx.Where("id = ?", User.ID).Updates(&User).Error
 	if err != nil {
 		tx.Rollback()
-		return err
+		return errors.NewInfraError(err, User.ID)
 	}
 	tx.Commit()
-	return err
+	return nil
 }
 func (p userPersistence) Get(ID int) (*entity.User, error) {
 	Db := database.Db
 	var user entity.User
 	err := Db.Model(user).Where("id = ?", ID).Find(&user).Error
-
-	return &user, err
+	return &user, errors.NewInfraError(err, ID)
 }
 func (p userPersistence) GetByLoginID(LoginID string) (*entity.User, error) {
 	Db := database.Db
 	var user entity.User
 	err := Db.Model(user).Where("login_id = ?", LoginID).Find(&user).Error
 	if err != nil {
-		log.Println(err)
-		return nil, err
+		return nil, errors.NewInfraError(err, LoginID)
 	}
-
-	return &user, err
+	return &user, nil
 }
 func (p userPersistence) GetByLoginIDAndPassword(LoginID string, Password value.Password) (*entity.User, error) {
 	Db := database.Db
 	user := new(entity.User)
 	err := Db.Model(*user).Where("login_id = ?", LoginID).Where("password = ?", Password).Find(user).Error
 	if err != nil {
-		log.Println(err)
-		return nil, err
+		return nil, errors.NewInfraError(err, LoginID, Password)
 	}
-	return user, err
+	return user, nil
 }
