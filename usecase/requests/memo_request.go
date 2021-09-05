4949101eff67b7bd94e607c17d1a75e07aec2fc6
ipe <izaki.yb1998@gmail.com>
2021-09-05 20:19:50 +0900

    requests package

---
+++ usecase/requests/memo_request.go
@@ -0,0 +1,18 @@
+package requests
+
+type CreateMemoRequest struct {
+	StartDate int64 `json:"start_date"`
+	EndDate   int64 `json:"end_date"`
+}
+type UpdateMemoRequest struct {
+	ID        int   `json:"id" binding:"required"`
+	StartDate int64 `json:"start_date"`
+	EndDate   int64 `json:"end_date"`
+}
+type GetMemoListRequest struct {
+	UserID int `json:"user_id"`
+}
+type GetMemoRequest struct {
+	ID     int `json:"id" binding:"required"`
+	UserID int
+}
