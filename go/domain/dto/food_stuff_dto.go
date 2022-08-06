a1726732d910ddaf25830ba851f6980843c869e0
ipe <izaki.yb1998@gmail.com>
2022-08-06 22:07:04 +0900

    FoodStuffdto修正

--- go/domain/dto/food_stuff_dto.go
+++ go/domain/dto/food_stuff_dto.go
@@ -6,11 +6,3 @@ type FoodStuff struct {
 	Name      string `json:"name"`
 	BuyStatus int    `json:"buy_status"`
 }
-
-
-type FoodStuff struct {
-	ID        int    `json:"id"`
-	MemoID    int    `json:"memo_id"`
-	Name      string `json:"name"`
-	BuyStatus int    `json:"buy_status"`
-}
